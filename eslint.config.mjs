ece958a9dd7b6622ae575b4a6ef89b1e36de50be
MrPinacolada <vasiasko112@gmail.com>
2024-11-04 19:38:27 +0300

    update docs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,6 @@
-// import pluginVue from "eslint-plugin-vue";
-// export default [
-//   ...pluginVue.configs["flat/recommended"],
-//   {
-//     rules: {},
-//   },
-// ];
-// @ts-check
-
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginVue from "eslint-plugin-vue";
-
-// export default [
-//   // Рекомендуемые правила для Vue
- 
-
-//   {
-   
-//     // Дополнительные настройки и правила
-//     rules: {
-//       "vue/no-unused-vars": "error", // Убираем неиспользуемые переменные в Vue
-//       // Добавьте другие правила, если нужно
-//     },
-//   },
-// ];
 
 export default tseslint.config(
   eslint.configs.recommended,
