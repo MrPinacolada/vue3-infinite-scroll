1f18e9acba963a6b2f69cd46801cf98d66442378
MrPinacolada <vasiasko112@gmail.com>
2024-11-04 19:53:07 +0300

    update docs

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="users">
-    <button class="docs-butt"></button>
+    <button class="docs-butt">asda</button>
     <!-- User cards displayed here -->
     <section ref="scrollContainer" class="users__container">
       <user-card v-for="user in users" :key="user.email" :user="user" />
