69ace59ccbcd65dfc71922722aeea70a8050c3f3
MrPinacolada <vasiasko112@gmail.com>
2024-11-04 16:16:43 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ foo }}
+  </div>
+  <button @click="foo += 1">asdasd</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const foo = ref<number>(0);
+</script>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+console.log('asdas');
+createApp(App).mount('#app');
